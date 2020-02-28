5434da6e07d2507d7c82535e841003c60a1cf7d0
Julie Greene <julie_greene22@yahoo.com>
2020-02-28 15:27:36 -0700

    basic model and users set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root 'boards#index'
+
+  # resources :users do 
+  #   resources :boards end 
+
+  resources :boards do
+    resources :lists end 
+
+  resources :lists do 
+    resources :tasks end 
+  
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
