c6fc43ce3cac42b27e0cc0f1b4358f394c4cf32f
Scott Key <scottdkey@gmail.com>
2020-02-29 14:14:10 -0700

    tasks crud actions done - working on making sense of the data

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,54 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy, :show]
+  before_action :set_list
+
+  def index
+    @tasks = @list.tasks
+  end
+
   def new
+    @task = @list.tasks.new
+  end
+
+  def create
+    @task = @list.tasks.new(task_params)
+    if @task.save
+      redirect_to list_tasks_path(@list)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def index
+  def update
+    if @task.update(task_params)
+      redirect_to list_tasks_path(@list)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to list_tasks_path
   end
 
   def show
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def task_params
+    params.require(:task).permit(:bullet, :priority)
   end
 end
